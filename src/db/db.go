75fc8d898785c76c49a07768b414009596ff7d49
Brian Feaster <brian@freelogue.com>
2014-06-13 14:40:52 -0700

    Run gofmt.

--- src/db/db.go
+++ src/db/db.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-"fmt"
-//"log"
-//"database/sql"
+	"fmt"
+	//"log"
+	//"database/sql"
 )
 
-func main () {
-   //rows, err := db.Query("");
-   fun()
+func main() {
+	//rows, err := db.Query("");
+	fun()
 }
 
-func fun () {
-   if a := 9 ; a == 9 {
-      fmt.Println("a =", a);
-   }
+func fun() {
+	if a := 9; a == 9 {
+		fmt.Println("a =", a)
+	}
 
-   for i, c := range "abcd" {
-      fmt.Println("idx:", i, " char:", c);
-   }
+	for i, c := range "abcd" {
+		fmt.Println("idx:", i, " char:", c)
+	}
 
-
-   for i, j := 0,9 
-       i<=j
-       i,j = i+2,j+1 {
-      fmt.Print("[i:", i, " j:", j, "]");
-   }
+	for i, j := 0, 9; i <= j; i, j = i+2, j+1 {
+		fmt.Print("[i:", i, " j:", j, "]")
+	}
 }
