de315d61de2b14fe5968eadb20b9f6d5fdc64712
brianfeaster <brian@freelogue.com>
2014-06-16 16:19:01 -0700

    Create table and add entry to postgres DB.

--- src/db/db.go
+++ src/db/db.go
@@ -22,32 +22,49 @@ func fun() {
 	}
 }
 
-func createTable () *DB {
+func openDatabase () *sql.DB {
 	db, err := sql.Open("postgres", "host=localhost dbname=freelogue sslmode=disable")
 	if err != nil { fmt.Println("ERROR: sql.Open()", err) }
 	return db
 }
 
+func createTable (db *sql.DB) {
+	var s = "create table data (name char (20) primary key, age integer)"
+	stmt, err := db.Prepare(s);
+	if err != nil { fmt.Println("ERROR:db.Prepare() ", err) }
+	res, err := stmt.Exec();
+	if err != nil || res == nil { fmt.Println("ERROR: stmt.Exec()", err) }
+	stmt.Close();
+}
+
+func addRandomPerson (db *sql.DB) {
+	var s = "insert into data values (" + "'brian feaster'" + ", " + "42" + ")";
+	fmt.Println(s);
+	stmt, err := db.Prepare(s);
+	if err != nil { fmt.Println("ERROR:db.Prepare() ", err) }
+
+	res, err := stmt.Exec();
+	if err != nil || res == nil { fmt.Println("ERROR: stmt.Exec()", err) }
+	stmt.Close();
+}
+
 func main() {
 	//fun()
+
+	fmt.Println(time.Now());
 
 	//fmt.Println()
 	//fmt.Println(db)
 	//fmt.Println(err)
 
-	db := createTable()
+	db := openDatabase()
 
-	var s = "create table data (name char (20) primary key, age integer)"
-	stmt, err := db.Prepare(s);
-	if err != nil { fmt.Println("ERROR:db.Prepare() ", err) }
+	if db != nil {
+		createTable(db)
+		addRandomPerson(db)
+	}
 
-	fmt.Println(time.Now());
-	res, err := stmt.Exec();
-	if err != nil { fmt.Println("ERROR: stmt.Exec()", err) }
 
-	fmt.Println(res);
-
-	stmt.Close();
 	db.Close()
 }
 
